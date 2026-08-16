2f33c0fee00cbef5a84036f88aaaf350f8c00278
Hope-Kathomi <hopekathomi@gmail.com>
2026-08-16 23:46:34 +0300

    final commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+// import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header.jsx"
+import About from "./About.jsx"
+import ArticleList from "./ArticleList.jsx";
 
-console.log(blogData);
+console.log("Blog: ",blogData);
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header about = {blogData.about}/>
+      <About image = {blogData.image}
+        about = {blogData.name}
+      />
+      <ArticleList posts = {blogData.posts}/>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+const Header = (name) => {
+  return (
+    <>
+    <h1>{name.about}</h1>
+    </>
+  )
+}
+
+export default Header
