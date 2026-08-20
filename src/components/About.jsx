d60c224eeab3d19905078369f83578975dc4ecbf
Hope-Kathomi <hopekathomi@gmail.com>
2026-08-20 13:59:33 +0300

    all tests passed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 // import React from 'react'
 
-const About = ({image,about}) => {
+const About = ({image = "https://via.placeholder.com/215",about}) => {
   return (
     <aside>
       <img src={image} alt="blog logo" />
